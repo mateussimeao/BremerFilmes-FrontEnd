33f9d775766004932a95b8a3f8e220af591ead53
Marcelo Silveira Guimarães Filho <marcel0guima2001@gmail.com>
2024-11-07 14:28:32 -0300

    Adapting to Bootstrap
    Adapting the Home page to bootstrap to be responsive,

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,79 +1,80 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/navbar/Navbar';
 import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
   const [searchResults, setSearchResults] = useState([]);
   const [query, setQuery] = useState('');
-  const [loading, setLoading] = useState(false); 
+  const [loading, setLoading] = useState(false);
 
-  const apiKey = '45eb858eef4393990a83b95485543080'; 
-  const apiUrl = 'https://api.themoviedb.org/3/search/movie'; 
+  const apiKey = '45eb858eef4393990a83b95485543080';
+  const apiUrl = 'https://api.themoviedb.org/3/search/movie';
 
   const handleSearch = async (e) => {
     e.preventDefault();
-    setLoading(true); 
+    setLoading(true);
     try {
       const res = await fetch(`${apiUrl}?api_key=${apiKey}&query=${query}`);
       const data = await res.json();
-      
-      // Limitar o número de filmes a 5
-      const limitedResults = data.results.slice(0, 8);
-
-      setSearchResults(limitedResults); 
+      const limitedResults = data.results.slice(0, 4);
+      setSearchResults(limitedResults);
     } catch (error) {
       console.error("Erro ao buscar filmes:", error);
     } finally {
-      setLoading(false); 
+      setLoading(false);
     }
   };
 
   return (
-    <div>
+    <div className="home-background text-light min-vh-100">
       <Navbar />
-      <p className="title-center">
-        My Bremer Box DB
-        <p className="slogan">Não se esqueça nunca quais seus filmes e atores preferidos</p>
-      </p>
+      <div className="container text-center mt-5">
+        <h1 className="display-4">My Bremer Box DB</h1>
+        <p className="lead">Não se esqueça nunca quais seus filmes e atores preferidos</p>
 
-      <div className="home">
-        {/* Barra de Pesquisa */}
-        <div className="search-bar">
-          <form onSubmit={handleSearch} className="search-bar">
+        <div className="my-4">
+          <form onSubmit={handleSearch} className="d-flex justify-content-center">
             <input
               type="text"
               placeholder="Buscar filmes..."
               value={query}
               onChange={(e) => setQuery(e.target.value)}
-              className="search-input"
+              className="form-control form-control-lg me-2"
+              style={{ maxWidth: '450px' }}
             />
-            <button type="submit" className="search-button">Buscar</button>
+            <button type="submit" className="btn btn-primary btn-lg w-25" style={{maxWidth:'200px'}}>Buscar</button>
           </form>
         </div>
 
-        {loading && <p>Carregando...</p>} 
+        {loading && <p>Carregando...</p>}
 
-        <div className="search-results">
+        <div className="mt-4">
           <h2>Resultados da busca:</h2>
-          <ul>
+          <div className="row mt-3">
             {searchResults.length > 0 ? (
               searchResults.map((movie, index) => (
-                <li key={index}>
-                  <h3>{movie.title}</h3>
-                  <div className="movie-overview">
-                    <p>{movie.overview}</p>
+                <div key={index} className="col-md-3 col-sm-6 mb-4">
+                  <div className="card h-100 bg-secondary text-light">
+                    <img
+                      src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                      className="card-img-top"
+                      alt={movie.title}
+                      style={{ maxHeight: '300px', objectFit: 'contain', width: '100%', marginTop: '10px' }}
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{movie.title}</h5>
+                      <p className="card-text movie-overview" style={{ maxHeight: '100px', overflowY: 'auto' }}>
+                        {movie.overview}
+                      </p>
+                    </div>
                   </div>
-                  <img 
-                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-                    alt={movie.title} 
-                    style={{ width: '150px' }}
-                  />
-                </li>
+                </div>
               ))
             ) : (
               <p>Nenhum filme encontrado.</p>
             )}
-          </ul>
+          </div>
         </div>
       </div>
     </div>
