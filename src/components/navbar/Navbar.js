8cb0aefc0abda975ebf589a35b0c99f8b69248e1
Rafael Luna <kenkorio23@gmail.com>
2024-11-07 23:10:34 -0300

    Added link to home in navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,45 +1,42 @@
 import React from 'react';
 import Logo from '../../img/logo.png';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-custom navbar-expand-lg navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">
-          <img src={Logo} alt="Logo" width="60" height="60" class="d-inline-block align-text-center"/>
+    <nav className="navbar navbar-custom navbar-expand-lg navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          <img src={Logo} alt="Logo" width="60" height="60" className="d-inline-block align-text-center"/>
           BremerBoxDB
         </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link" aria-current="page">Home</Link>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Filmes</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Perfil
               </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">Página de perfil</a></li>
-                <li><a class="dropdown-item" href="#">Minhas reviews</a></li>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Página de perfil</a></li>
+                <li><a className="dropdown-item" href="#">Minhas reviews</a></li>
               </ul>
             </li>
           </ul>
-          <form class="d-flex" role="search">
-            <input class="form-control me-2" type="search" placeholder="Pesquisar filme" aria-label="Search"/>
-            <button class="btn btn btn-outline-light" type="submit">Enviar</button>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Pesquisar filme" aria-label="Search"/>
+            <button className="btn btn btn-outline-light" type="submit">Enviar</button>
           </form>
         </div>
       </div>
     </nav>
   );
 }
-
 
 export default Navbar;
