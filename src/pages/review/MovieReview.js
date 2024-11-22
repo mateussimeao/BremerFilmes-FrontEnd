414ae4c91fe3fab51fbe4478eb2ef577a5957f80
Danilo Scheltes Cardoso <93267380+DanScheltes@users.noreply.github.com>
2024-11-22 15:20:46 -0300

    Arrumando o Review
    Coloquei a Navbar, arrumei umas cores.
    Ainda não consigo testar pois não tenho conseguido passar da pagina de login.

--- src/pages/review/MovieReview.js
+++ src/pages/review/MovieReview.js
@@ -1,6 +1,8 @@
-/*import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './MovieReview.css';
+import Navbar from '../../components/navbar/Navbar';
+
 
 const MovieReview = ({ movieId }) => {
   const [movieDetails, setMovieDetails] = useState(null);
@@ -35,6 +37,7 @@ const MovieReview = ({ movieId }) => {
 
   return (
     <div className="movie-review-container">
+      <Navbar />
       <div className="movie-poster">
         <img src={movieDetails.posterUrl} alt={`${movieDetails.title} poster`} />
       </div>
@@ -57,65 +60,3 @@ const MovieReview = ({ movieId }) => {
 };
 
 export default MovieReview;
-*/
-
-import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import "./MovieReview.css";
-
-const ReviewPage = () => {
-  const { id } = useParams();
-  const [movie, setMovie] = useState({});
-  const [reviews, setReviews] = useState([]);
-
-  useEffect(() => {
-    // Simulando os detalhes do filme
-    const mockMovie = {
-      title: "Inception",
-      rating: 8.8,
-      poster_path: "C:\Users\cardo\OneDrive\Imagens.jpg",
-      overview: "Um thriller de ficção científica que explora a mente humana através dos sonhos.",
-    };
-    setMovie(mockMovie);
-
-    // Simulando as reviews dos usuários
-    const mockReviews = [
-      {
-        user: "Usuário1",
-        review: "Excelente filme! A história é muito envolvente e os efeitos visuais são incríveis.",
-        rating: 9,
-      },
-      {
-        user: "Usuário2",
-        review: "Gostei muito, mas o final deixou algumas dúvidas. Ótima atuação dos atores.",
-        rating: 8,
-      },
-    ];
-    setReviews(mockReviews);
-  }, [id]);
-
-  return (
-    <div className="review-page">
-      <div className="movie-details">
-        <img src={movie.poster_path} alt={`${movie.title} Poster`} />
-        <div className="movie-info">
-          <h1>{movie.title}</h1>
-          <p style={{ marginTop: '10px' }}>Nota: {movie.rating}/10</p>
-          <p>{movie.overview}</p>
-        </div>
-      </div>
-      
-      <div className="reviews-section">
-        <h2>Reviews dos Usuários</h2>
-        {reviews.map((review, index) => (
-          <div key={index} className="review">  
-            <h3>{review.user}</h3>
-            <p>{review.review}</p>
-            <p>Nota: {review.rating}/10</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
-}  
-export default ReviewPage;
